32fd90c48c7c8441adb1a8077d4bfee79c463476
20WH1A05F7 <93469997+NaraPoojitha@users.noreply.github.com>
2024-04-26 12:58:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ if 'page' in st.session_state and st.session_state['page'] == 'fertilizer_recomm
     <style>
     [data-testid="stAppViewContainer"] {{
     background-color:#90EE90; /* changed to #90EE90 */
-    background-image: url("https://thumbs.dreamstime.com/b/fertilizer-use-isolated-cartoon-vector-illustrations-farmer-throws-ground-agribusiness-industry-agricultural-input-sector-297223018.jpg");
+    background-image: url("https://www.shutterstock.com/image-vector/farmer-activity-agricultural-concept-colored-260nw-2424129921.jpg");
     background-repeat: no-repeat;
     background-size:cover;
 
